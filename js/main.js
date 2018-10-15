aa5354ca6802cbed18bb2ce25b715411b1857d72
Yangyang Cui <cuiyang21@gmail.com>
2018-10-15 11:07:26 -0500

    all nav-menu have own sidebar

--- js/main.js
+++ js/main.js
@@ -8,9 +8,67 @@
 // }
 
 $(document).ready(() => {
-    $('#search').on('click', () => {
-        $('#side-search').toggleClass('main-sidebar-width');
-        $('#text-search').toggleClass('text-search-color');
-        $('#img-search').toggleClass('img-search-opacity');
+
+    /* nav-menu-search */
+    $('#nav-menu-search').on('click', () => {
+        /* navbar */
+        $('#nav-text-search').toggleClass('nav-text-color');
+        $('#nav-img-search').toggleClass('nav-img-opacity');
+        /* sidebar */
+        $('#sidebar-search').toggleClass('main-sidebar-width');
+
     });
-});
+
+    /* nav-menu-layers */
+    $('#nav-menu-layers').on('click', () => {
+        /* navbar */
+        $('#nav-text-layers').toggleClass('nav-text-color');
+        $('#nav-img-layers').toggleClass('nav-img-opacity');
+        /* sidebar */
+        $('#sidebar-layers').toggleClass('main-sidebar-width');
+
+    });
+
+    /* nav-menu-building */
+    $('#nav-menu-building').on('click', () => {
+        /* navbar */
+        $('#nav-text-building').toggleClass('nav-text-color');
+        $('#nav-img-building').toggleClass('nav-img-opacity');
+        /* sidebar */
+        $('#sidebar-building').toggleClass('main-sidebar-width');
+
+    });
+
+    /* nav-menu-direction */
+    $('#nav-menu-direction').on('click', () => {
+        /* navbar */
+        $('#nav-text-direction').toggleClass('nav-text-color');
+        $('#nav-img-direction').toggleClass('nav-img-opacity');
+        /* sidebar */
+        $('#sidebar-direction').toggleClass('main-sidebar-width');
+
+    });
+
+    /* nav-menu-tour */
+    $('#nav-menu-tour').on('click', () => {
+        /* navbar */
+        $('#nav-text-tour').toggleClass('nav-text-color');
+        $('#nav-img-tour').toggleClass('nav-img-opacity');
+        /* sidebar */
+        $('#sidebar-tour').toggleClass('main-sidebar-width');
+
+    });
+
+    /* nav-menu-account */
+    $('#nav-menu-account').on('click', () => {
+        /* navbar */
+        $('#nav-text-account').toggleClass('nav-text-color');
+        $('#nav-img-account').toggleClass('nav-img-opacity');
+        /* sidebar */
+        $('#sidebar-account').toggleClass('main-sidebar-width');
+
+    });
+
+
+
+}); /* End:  $(document).ready */

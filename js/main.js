0f031bce20358bb86771d2b3c80bf8cd131b55b4
Yangyang Cui <cuiyang21@gmail.com>
2018-10-28 18:21:40 -0500

    add XMLHttpRequest code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-// XMLHttpRequest
-let request = new XMLHttpRequest();
-request.open('get', '../json/layers.json', true);
-request.onload = function() {
-    console.log(request.responseText);
-}
-request.send();
 /* jQuery */
 $(document).ready(() => {
+
+    let jsonDataContainer = document.getElementById("js-sidebar");
+
+    $('.js-img').on("click",()=>{
+        // XMLHttpRequest
+        let request = new XMLHttpRequest();
+        request.open('get', 'http://localhost:8080/navbar/json/layers.json', true);
+        request.onload = function() {
+            var data = JSON.parse(request.responseText);
+            // console.log(data[0]);
+            renderHTML(data);
+        };
+        request.send();
+    });
+
+    function renderHTML(data) {
+        let jsonData = "", i;
+        for(i = 0; i < data.length; i++ ) {
+            jsonData += "<p>" + data[i].city + " is great.</p>"
+        }
+        jsonDataContainer.insertAdjacentHTML('beforeend', jsonData);
+    }
 
 
 

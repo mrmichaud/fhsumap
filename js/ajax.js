540210b734f4122a27999c8d52040ff618ea749b
Yangyang Cui <cuiyang21@gmail.com>
2018-10-29 23:16:15 -0500

    start to delete a href

--- js/ajax.js
+++ js/ajax.js
@@ -1,9 +1,24 @@
 let sidebarContainer = document.getElementById("js-sidebar");
 
 let request = new XMLHttpRequest();
-request.open('GET', 'json/real.json', true);
+request.open('GET', 'http://localhost:8080/navbar/json/real.json', true);
 request.onload = function() {
-    console.log(request.responseText);
-    //let data = request.responseText;
+
+    let data = JSON.parse(request.responseText);
+    console.log(data[0]);
 };
 request.send();
+
+
+/* Google Map */
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 38.8714,
+            lng: -99.3445
+        },
+        zoom: 18
+    });
+}

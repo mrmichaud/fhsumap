e1d6fdbcad14d12361fd4e839eb3133a6657ac3c
Yangyang Cui <cuiyang21@gmail.com>
2018-11-03 09:48:00 -0500

    my code has some CSS problem

--- js/N-spa.js
+++ js/N-spa.js
@@ -1,26 +1,26 @@
-function loadContent(tab){
-    $('#sidebar').append('<div id="sidebar-'+tab+'" class="sidebar-content">'+tab+'</div>');
-    
+function loadContent(tab) {
+    $('#sidebar').append('<div id="sidebar-' + tab + '" class="sidebar-content">' + tab + '</div>');
+
 }
 
-function showContent(tab){
+function showContent(tab) {
     $('.sidebar-content').hide();
-    if($('#sidebar').find('#sidebar-'+tab).html()==undefined) loadContent(tab);
-    else $('#sidebar-'+tab).show();
+    if ($('#sidebar').find('#sidebar-' + tab).html() == undefined) loadContent(tab);
+    else $('#sidebar-' + tab).show();
 }
 
-var currentTab='layers';
-var tabToggle=true;
-$(".nav-sb-menu[data-label='layers']").css('color','white');
-$('.nav-sb-menu').on('click',function(){
-    $('.nav-sb-menu').css('color','black');
-    if(currentTab!=$(this).data('label') || !tabToggle){
-        $(".nav-sb-menu[data-label='" + $(this).data('label') +"']").css('color','white');
+var currentTab = 'layers';
+var tabToggle = true;
+$(".nav-sb-menu[data-label='layers']").css('color', 'white');
+$('.nav-sb-menu').on('click', function() {
+    $('.nav-sb-menu').css('color', 'black');
+    if (currentTab != $(this).data('label') || !tabToggle) {
+        $(".nav-sb-menu[data-label='" + $(this).data('label') + "']").css('color', 'white');
         showContent($(this).data('label'));
     }
-    if(currentTab==$(this).data('label') || !tabToggle){
-        $('.sidebar').animate({width:"toggle"});
-        tabToggle=!tabToggle;
+    if (currentTab == $(this).data('label') || !tabToggle) {
+        $('.sidebar').animate({ width: "toggle" });
+        tabToggle = !tabToggle;
     }
-    currentTab=$(this).data('label');
+    currentTab = $(this).data('label');
 });

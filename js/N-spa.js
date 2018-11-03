c6feddad7d5b4b7015d6c8acf0bc34bc34384f80
Yangyang Cui <cuiyang21@gmail.com>
2018-11-03 11:14:44 -0500

    navbar all are mess

--- js/N-spa.js
+++ js/N-spa.js
@@ -8,7 +8,7 @@ function showContent(tab) {
     if ($('#sidebar').find('#sidebar-' + tab).html() == undefined) loadContent(tab);
     else $('#sidebar-' + tab).show();
 }
-
+/* Toggle sidebar */
 var currentTab = 'layers';
 var tabToggle = true;
 $(".nav-sb-menu[data-label='layers']").css('color', 'white');
@@ -24,3 +24,16 @@ $('.nav-sb-menu').on('click', function() {
     }
     currentTab = $(this).data('label');
 });
+
+/* Google Map */
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 38.8714,
+            lng: -99.3445
+        },
+        zoom: 18
+    });
+}

cbbfdf901f29ddae7c1a9128c3d498416142e662
Ella Ding <m_ding6@mail.fhsu.edu>
2018-12-07 14:43:54 -0600

    modified getBuildingID  in direction.js

--- js/directions.js
+++ js/directions.js
@@ -16,23 +16,7 @@ function showWalkingDirections() {
 	// Display the route between the initial start and end selections.
 	calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map);
 }
-
-var start, end;
-        if(document.getElementById('walkingDirectionsStartInput').value==0){
-          start = currentPosition;
-        }
-        //else start = getBuildingID(document.getElementById('walkingDirectionsStartInput').value).latLngMainEntrance;
-        else start = DataTypesInformation[document.getElementById('walkingDirectionsStartInput').value].latLngMainEntrance;
-       alert(start);
-
-        if(document.getElementById('walkingDirectionsEndInput').value==0){
-          end = currentPosition;
-        }
-
-        //else start = getBuildingID(document.getElementById('walkingDirectionsEndInput').value).latLngMainEntrance;
-        else end = DataTypesInformation[document.getElementById('walkingDirectionsEndInput').value].latLngMainEntrance;
-        //console.log(end);
-
+/*
 function calculateAndDisplayRoute(directionsDisplay, directionsService,	markerArray, stepDisplay, map) {
 	
 	//connects directionsDisplay back to the map again
@@ -45,7 +29,7 @@ function calculateAndDisplayRoute(directionsDisplay, directionsService,	markerAr
 		markerArray[i].setMap(null);
 	}
 	// Retrieve the start and end locations and create a DirectionsRequest using WALKING directions.
-	/*
+	
         var start, end;
         if(document.getElementById('walkingDirectionsStartInput').value==0){
           start = currentPosition;
@@ -61,7 +45,7 @@ function calculateAndDisplayRoute(directionsDisplay, directionsService,	markerAr
         //else start = getBuildingID(document.getElementById('walkingDirectionsEndInput').value).latLngMainEntrance;
         else end = DataTypesInformation[document.getElementById('walkingDirectionsEndInput').value].latLngMainEntrance;
         //console.log(end);
-	 */   
+	    
 	    directionsService.route(
 		{
 			origin: end,
@@ -106,7 +90,67 @@ function calculateAndDisplayRoute(directionsDisplay, directionsService,	markerAr
 		}
 	);
 }
+*/
 
+function calculateAndDisplayRoute(directionsDisplay, directionsService,	markerArray, stepDisplay, map) {
+	
+	//connects directionsDisplay back to the map again
+	directionsDisplay.setMap(map);
+	directionsDisplay.setPanel(document.getElementById('walkingStepsContainer'));
+	
+	// First, remove any existing markers from the map.
+	//Basically hide function
+	for (var i = 0; i < markerArray.length; i++) {
+		markerArray[i].setMap(null);
+	}
+	// Retrieve the start and end locations and create a DirectionsRequest using WALKING directions.
+	
+	directionsService.route(
+		{
+			origin: getBuildingID(document.getElementById('walkingDirectionsStartInput').value).latLngMainEntrance,
+			destination: getBuildingID(document.getElementById('walkingDirectionsEndInput').value).latLngMainEntrance,			
+			//origin: DataTypesInformation[document.getElementById('walkingDirectionsStartInput').value].latLngMainEntrance,
+			//destination: DataTypesInformation[document.getElementById('walkingDirectionsEndInput').value].latLngMainEntrance,
+			travelMode: 'WALKING'
+		}, 
+		
+		function(response, status) {
+			// Route the directions and pass the response to a function to create markers for each step.
+			
+			if (status === 'OK') {
+				
+				var startAndEnd = {
+					url: "siteImages/gold_pin.png",
+					scaledSize: new google.maps.Size(40,40)
+				}
+				
+				//document.getElementById("warnings-panel").innerHTML = '<b>' + response.routes[0].warnings + '</b>';
+				//alert(response.routes[0].warnings);
+				console.log(response);
+				directionsDisplay.setDirections(response);
+				
+				var startMarker = new google.maps.Marker({
+                    position: getBuildingID(document.getElementById('walkingDirectionsStartInput').value).latLngMainEntrance,
+                    map: map,
+                    icon: startAndEnd
+                });
+				startEndMarkerArray.push(startMarker);
+				
+				var endMarker = new google.maps.Marker({
+                    position: getBuildingID(document.getElementById('walkingDirectionsEndInput').value).latLngMainEntrance,
+                    map: map,
+                    icon: startAndEnd
+                });
+				startEndMarkerArray.push(endMarker);
+				
+				showSteps(response, markerArray, stepDisplay, map);
+			} 
+			else {
+				window.alert('Directions request failed due to ' + status);
+			}
+		}
+	);
+}
 function showSteps(directionResult, markerArray, stepDisplay, map) {
 	// For each step, place a marker, and add the text to the marker's infowindow.
 	// Also attach the marker to an array so we can keep track of it and remove it
